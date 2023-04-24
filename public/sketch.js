2f227b8f8976181920af18f25d87727280ee3289
José Alexandre Soares de Oliveira <josesoaresoliveira@usp.br>
2023-04-24 12:26:13 -0300

    Bug fixed

--- public/sketch.js
+++ public/sketch.js
@@ -4,32 +4,34 @@ var button;
 var xCoords = [];
 
 function preload() {
-  song = loadSound('sample/caminho-do-crusp-ônibus.mp3');
+  song = loadSound('sample/picapau.mp3');
 }
 
-function setup () {
+function setup() {
   createCanvas(1024, 1024);
   button = createButton('Play');
-  button.mousePressed(toggle);
-  fft = new p5.FFT(0.9, 32);
-  // Initialize x-coordinates array
-  var spacing = height / 32; // distribute the lines equally across the canvas
-  for (var i = 0; i < 32; i++) { // Distribute the lines
+  button.mousePressed(playSong);
+  fft = new p5.FFT(0.9, 64);
+  fft.smooth(0.9);
+  var spacing = height / 64;
+  for (var i = 0; i < 64; i++) {
     xCoords[i] = [];
     for (var j = 0; j < width; j++) {
-      xCoords[i][j] = j
+      xCoords[i][j] = j;
     }
   }
+  setInterval(draw, 1000 / 60);
 }
 
-function toggle() {
+function playSong() {
   if (song.isPlaying()) {
+    button.html('Play');
     song.pause();
-    noLoop(); // Stop the animation
-  }
-  else {
+    noLoop();
+  } else {
+    button.html('Stop');
     song.play();
-    loop(); // Resume the animation
+    loop();
   }
 }
 
@@ -37,36 +39,36 @@ function draw() {
   if (song.isPlaying()) {
     background(0);
     noFill();
+    stroke(255);
+    strokeWeight(1);
     var spectrum = fft.analyze();
-    var spacing = height / 32;
-
-    for (var i = 0; i < 32; i++) {
-      var amplitude = spectrum[i];
-      var y = (spacing * i) + (height / 64); // reverse y-axis and spacing
-      var distortionFactor = 3.2;
-      var yDistortion = map(distortionFactor * amplitude, 0, 255, -spacing / 2, spacing / 2);
+    var spacing = height / 64;
+    for (var i = 0; i < 128; i++) {
+      var amplitude = spectrum[i] * 10;
+      var y = (spacing * i) + (height / 32);
+      var distortionFactor = 1;
+      var yDistortion = map(amplitude, 0, 255, spacing * distortionFactor, -spacing * distortionFactor);
       y += yDistortion;
-
       var time = song.currentTime();
       var x = map(time, 0, song.duration(), 0, width);
-      xCoords[i].push({x: x, y: y});
-
+      xCoords[i].push({
+        x: x%width,
+        y: y
+      });
+      if (xCoords[i].length > 1 && xCoords[i][xCoords[i].length - 1].x < 0) {
+        xCoords[i] = [];
+      }
       if (xCoords[i].length > 1) {
-        stroke(255);
-        strokeWeight(2);
-        noFill();
         beginShape();
-        for (var j = 0; j < xCoords[i].length; j++) {
-          vertex(xCoords[i][j].x, xCoords[i][j].y);
+        curveVertex(xCoords[i][0].x, xCoords[i][0].y);
+        for (var j = 1; j < xCoords[i].length; j++) {
+          curveVertex(xCoords[i][j].x, xCoords[i][j].y);
         }
         endShape();
-      }
-
-      while (xCoords[i].length > 0 && xCoords[i][0].x < 0) {
-        xCoords[i].shift();
       }
     }
   } else {
     noLoop();
+    button.html('Play')
   }
 }
